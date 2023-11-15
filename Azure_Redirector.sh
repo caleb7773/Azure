8c27e9e15da87c571132f3bc7d72a2d86d99949f
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2023-11-15 11:27:15 -0500

    Update Azure_Redirector.sh

--- Azure_Redirector.sh
+++ Azure_Redirector.sh
@@ -240,7 +240,7 @@ echo "##########################################################################
 	then
 		PROJECT=twister
 	fi
-	RESOURCE_GROUP=${SUBSCRIPTION_NAME}-${REGION}-${PROJECT}-rg
+	RESOURCE_GROUP=${SUBSCRIPTION_NAME}-${REGION}-${PROJECT}-TP-rg
    	echo
  	echo "Validating Resource Group Choice..."
 	output=$(az group show --name ${RESOURCE_GROUP} 2> /dev/null)
