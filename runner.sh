8a9a507e7b2cd914cb9a7fd691a5b7c297ba577a
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2023-11-22 19:58:50 -0500

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo 
+project_code=${RANDOM}
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
+
+
+# Kill the script if someone runs it as root
+
+if [[ $(id -u) == 0 ]];
+then
+	echo
+	echo "Do not run this as root you joker!"
+	exit 1
+fi
+
+clear
+echo
+echo -e "${GREEN}
